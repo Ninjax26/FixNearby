fd7304ae272e10ba9e25973017b5f4ad5eb01b6e
KGFCH2 <babinbid05@gmail.com>
2026-06-03 09:45:50 +0530

    feat: add global ThemeProvider context for consistent dark theme support

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,16 +5,19 @@ import App from './App.jsx'
 import { LocationProvider } from './context/LocationContext.jsx'
 import { ToastProvider } from './context/ToastContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <LocationProvider>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      </LocationProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <LocationProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </LocationProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
